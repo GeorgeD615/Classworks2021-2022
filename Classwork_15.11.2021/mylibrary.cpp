13c36f5019ba3f9ee9a30ac3f84003ce0e807cad
GeorgeD615 <st095277@student.spbu.ru>
2021-11-22 11:24:53 +0300

    task

--- Classwork_15.11.2021/mylibrary.cpp
+++ Classwork_15.11.2021/mylibrary.cpp
@@ -1,56 +1,46 @@
 #include <iostream>
 #include "mylibrary.h"
-int** create2DArray(int r, int c) {//Создание двумерного массива
-	int** array;
-	array = new int* [r];
+
+int** create2DArray(int r, int c) {//Создание 2-массива с заданными размерами.
+	int **array = new int* [r];
 	for (int i = 0; i < r; ++i) {
 		array[i] = new int[c];
 	}
 	return array;
 }
-void delete2DArray(int** mas, int n, int m)//Очистка памяти от двумерного массива
+void delete2DArray(int** mas, int n, int m)//Освобождение ресурсов от двумерного массива
 {
 	for (int i = 0; i < n; i++)
 		delete[] mas[i];
 	delete[] mas;
 }
-void fillArrayRandomly(int* a, int size) {//Заполнение одномерного массива случайными элементами 
-	for (int i = 0; i < size; ++i) {
-		a[i] = rand() % 10;
-	}
-}
-void fill2DArrayRandomly(int** a, int rows, int cols) {//Заполнение двумерного массива случайными элементами 
+void fill2DArrayRandomly(int** a, int rows, int cols) {//Заполнение массива случайными элементами
 	for (int i = 0; i < rows; ++i) {
-		fillArrayRandomly(a[i], cols);
+		for (int j = 0; j < cols; ++j) {
+			a[i][j] = rand() % 10;
+		}
 	}
-}
-template<class Type>
-void printArray(Type array, size_t size) {//Вывод одномерного массива
-	for (int i = 0; i < size; ++i) {
-		std::cout << array[i] << " ";
-	}
-	std::cout << std::endl;
-}
-void print2Darray(int** array, int r, int c) {//Вывод двумерного массива 
-	for (int x = 0; x < r; ++x) {
-		printArray(array[x], c);
-	}
-	std::cout << std::endl;
 }
 
-void fillArrayN(int* a, int size, int n) {//Заполнение одномерного массива константным значением
-	for (int i = 0; i < size; ++i) {
-		a[i] = n;
+void print2Darray(int** array, int r, int c) {// Вывод двумерного массива 
+	for (int i = 0; i < r; ++i) {
+		for (int j = 0; j < c; ++j) {
+			std::cout << array[i][j] << ' ';
+		}
+		std::cout << std::endl;
 	}
 }
 void fill2DArrayN(int** a, int rows, int cols, int n) {//Заполнение двумерного массива константным значением
 	for (int i = 0; i < rows; ++i) {
-		fillArrayN(a[i], cols, n);
+		for (int j = 0; j < cols; ++j) {
+			a[i][j] = n;
+		}
 	}
 }
-void fill2DArrayDiag1(int** mas, int n, int m) {//Заполнение массива единицами по главной диагонали и нулями по всем остальным элементам
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < m; ++j) {
+int** create2DArrayDiag1(int rc) {//Инициализация 2-массива с 1 на главной диагонали и 0 в остальных элементах.
+	int** mas = create2DArray(rc, rc);
+	for (int i = 0; i < rc; ++i) {
+		for (int j = 0; j < rc; ++j) {
 			if (i == j) {
 				mas[i][j] = 1;
 			}
@@ -59,21 +49,18 @@ void fill2DArrayDiag1(int** mas, int n, int m) {//Заполнение массива единицами п
 			}
 		}
 	}
+	return mas;
 }
-int** createTrans2DArray(int** mas, int r, int c) { //Создание двумерного транспонированного массива
-	int** array;
-	array = new int* [c];
+int** createTrans2DArray(int** mas, int r, int c) { //Создание транспанированной матрицы
+	int** array = create2DArray(c, r);
 	for (int i = 0; i < c; ++i) {
-		array[i] = new int[r];
-	}
-	for (int i = 0; i < r; ++i) {
-		for (int j = 0; j < c; ++j) {
-			array[j][i] = mas[i][j];
+		for (int j = 0; j < r; ++j) {
+			array[i][j] = mas[j][i];
 		}
 	}
 	return array;
 }
-int** create1802DArray(int** mas, int r, int c) { //Модификация исходного 2-массива в виде поворота на 180 градусов.
+int** create1802DArray(int** mas, int r, int c) {//Создание матрицы повёрнутой на 180 градусов
 	int** array;
 	array = new int* [r];
 	for (int i = 0; i < r; ++i) {
@@ -81,20 +68,20 @@ int** create1802DArray(int** mas, int r, int c) { //Модификация исходного 2-масс
 	}
 	for (int i = 0; i < r; ++i) {
 		for (int j = 0; j < c; ++j) {
-			array[i][j] = mas[r-i-1][c-j-1];
+			array[i][j] = mas[r - i - 1][c - j - 1];
 		}
 	}
 	return array;
 
 }
-void MultArr_By_number(int** mas, int n, int m, int k) {//Умножение массива на число
+void MultArr_By_number(int** mas, int n, int m, int k) {//Умножение матрицы на число
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < m; ++j) {
-				mas[i][j] *= k;
+			mas[i][j] *= k;
 		}
 	}
 }
-int** createMultArr_By_number(int** mas, int r, int c, int k) { //Создание матрицы, как результата умножения матрицы на число.
+int** createMultArr_By_number(int** mas, int r, int c, int k) { //Создание матрицы умноженной на число
 	int** array;
 	array = new int* [r];
 	for (int i = 0; i < r; ++i) {
@@ -102,13 +89,13 @@ int** createMultArr_By_number(int** mas, int r, int c, int k) { //Создание матри
 	}
 	for (int i = 0; i < r; ++i) {
 		for (int j = 0; j < c; ++j) {
-			array[i][j] = mas[i][j]*k;
+			array[i][j] = mas[i][j] * k;
 		}
 	}
 	return array;
 }
 
-int** createArr_addition(int** mas1, int** mas2, int r, int c) { //Создание матрицы как результата сложения двух числовых матриц.
+int** createArr_addition(int** mas1, int** mas2, int r, int c) { //Создание матрицы, как суммы двух матриц
 	int** array;
 	array = new int* [r];
 	for (int i = 0; i < r; ++i) {
@@ -122,7 +109,7 @@ int** createArr_addition(int** mas1, int** mas2, int r, int c) { //Создание матр
 	return array;
 }
 
-int** createArr_mult(int** mas1, int** mas2, int r1, int c1, int c2) { //Создание матрицы как результата умножения двух числовых матриц.
+int** createArr_mult(int** mas1, int** mas2, int r1, int c1, int c2) { //Создание матрицы, как произведения двух матриц
 	int** array;
 	array = new int* [r1];
 	for (int i = 0; i < r1; ++i) {
@@ -130,7 +117,7 @@ int** createArr_mult(int** mas1, int** mas2, int r1, int c1, int c2) { //Создани
 	}
 	for (int i = 0; i < r1; ++i) {
 		for (int j = 0; j < c2; ++j) {
-				array[i][j] = 0;
+			array[i][j] = 0;
 		}
 	}
 	for (int i = 0; i < r1; ++i) {
@@ -142,26 +129,29 @@ int** createArr_mult(int** mas1, int** mas2, int r1, int c1, int c2) { //Создани
 	}
 	return array;
 }
-void swapLines(int** mas, int n, int m, int n1, int n2) { //Обмен местами двух строк 2-массива.
+void swapLines(int** mas, int n, int m, int n1, int n2) { // Поменять местами строки в массиве
 	int tmp = 0;
 	for (int i = 0; i < m; ++i) {
-		tmp = mas[n1-1][i];
-		mas[n1-1][i] = mas[n2-1][i];
-		mas[n2-1][i] = tmp;
+		tmp = mas[n1 - 1][i];
+		mas[n1 - 1][i] = mas[n2 - 1][i];
+		mas[n2 - 1][i] = tmp;
 	}
 }
-void swapСolumns(int** mas, int n, int m, int n1, int n2) { //Обмен местами двух строк 2-массива.
+void swapColumns(int** mas, int n, int m, int n1, int n2) { // Поменять местами столбцы в массиве
 	int tmp = 0;
 	for (int i = 0; i < n; ++i) {
-		tmp = mas[i][n1-1];
-		mas[i][n1-1] = mas[i][n2-1];
-		mas[i][n2-1] = tmp;
+		tmp = mas[i][n1 - 1];
+		mas[i][n1 - 1] = mas[i][n2 - 1];
+		mas[i][n2 - 1] = tmp;
 	}
 }
 
-int& GetArrElement(int** mas, int n, int m) {
+int& GetArrElement(int** mas, int n, int m) { //Получить элемент массива по индексам
 	return mas[n - 1][m - 1];
 }
+
+
+
 
 
 
--- Classwork_15.11.2021/mylibrary.h
+++ Classwork_15.11.2021/mylibrary.h
@@ -1,15 +1,12 @@
 #ifndef _My_library_2021_
 #define _My_library_2021_
+
 int** create2DArray(int r, int c);
 void delete2DArray(int** mas, int n, int m);
-void fillArrayRandomly(int* a, int size);
 void fill2DArrayRandomly(int** a, int rows, int cols);
-template<class Type>
-void printArray(Type array, size_t size);
 void print2Darray(int** array, int r, int c);
-void fillArrayN(int* a, int size, int n);
 void fill2DArrayN(int** a, int rows, int cols, int n);
-void fill2DArrayDiag1(int** mas, int n, int m);
+int** create2DArrayDiag1(int rc);
 int** createTrans2DArray(int** mas, int r, int c);
 int** create1802DArray(int** mas, int r, int c);
 void MultArr_By_number(int** mas, int n, int m, int k);
@@ -17,7 +14,6 @@ int** createMultArr_By_number(int** mas, int r, int c, int k);
 int** createArr_addition(int** mas1, int** mas2, int r, int c);
 int** createArr_mult(int** mas1, int** mas2, int r1, int c1, int c2);
 void swapLines(int** mas, int n, int m, int n1, int n2);
-void swapÑolumns(int** mas, int n, int m, int n1, int n2);
+void swapColumns(int** mas, int n, int m, int n1, int n2);
 int& GetArrElement(int** mas, int n, int m);
 #endif 
-
